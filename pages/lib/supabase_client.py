662dbef3967ae60956c58bb962e120d6aeea9996
462Caleb <1103cmy@gmail.com>
2026-03-03 19:07:17 -0600

    Delete pages directory

--- pages/lib/supabase_client.py
+++
@@ -1,8 +0,0 @@
-import os
-from supabase import create_client, Client
-
-def get_supabase() -> Client:
-    return create_client(os.environ["SUPABASE_URL"], os.environ["SUPABASE_ANON_KEY"])
-
-def creator_email() -> str:
-    return os.environ.get("CREATOR_EMAIL", "").strip().lower()
